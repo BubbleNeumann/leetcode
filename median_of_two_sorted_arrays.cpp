45470983dc0faeacbb824692098ffc5c800915f2
dana <bubbleneumann@gmail.com>
2024-03-15 13:54:23 +0400

    add template

---
+++ median_of_two_sorted_arrays.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+class Solution {
+public:
+    double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
+
+        return 0;
+        
+    }
+};
+
+
+int maint() {
+    return 0;
+}
